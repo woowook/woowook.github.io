d27811afef89e7c7e511a6a86eec81fdca354e56
wook <wook@neowiz.com>
2022-08-17 07:04:21 +0900

    image directory change

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 /* eslint-env browser */
 
 const body = document.querySelector("body");
-const images = ["0.jpg", "1.jpg", "2.jpg"];
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
 
 // 이미지 랜덤으로 불러오기
 const choseImage = images[Math.floor(Math.random() * images.length)];
@@ -10,7 +10,7 @@ const choseImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 // Console.log(bgImage);
 
-bgImage.src = `../image/${choseImage}`;
+bgImage.src = `./image/${choseImage}`;
 // Console.log(bgImage);
 bgImage.classList.add("bgImage");
 document.body.append(bgImage);
